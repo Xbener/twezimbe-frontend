1bca6effca068721b8e5e7c5302087f1a69377f5
henritresor <shimwamanahenritresor@gmail.com>
2024-10-16 15:02:06 +0200

    added minimal settings

--- src/app/groups/[groupId]/GroupChannelDetails.tsx
+++ src/app/groups/[groupId]/GroupChannelDetails.tsx
@@ -1,8 +1,13 @@
+import { useGetProfileData } from '@/api/auth'
+import { Button } from '@/components/ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { GroupContext } from '@/context/GroupContext'
+import { iconTextGenerator } from '@/lib/iconTextGenerator'
+import { Avatar, AvatarImage, AvatarFallback } from '@radix-ui/react-avatar'
 import { CaretDownIcon } from '@radix-ui/react-icons'
-import { PlusIcon, Settings } from 'lucide-react'
+import { Edit, LogOut, PlusIcon, Settings } from 'lucide-react'
 import Link from 'next/link'
+import { userInfo } from 'os'
 import React, { useContext } from 'react'
 
 type Props = {
@@ -13,9 +18,15 @@ type Props = {
 
 function ChannelDetails({ }: Props) {
     const { group } = useContext(GroupContext)
+    const { currentUser } = useGetProfileData()
 
     const menuItems = [
         { name: "Group settings", link: `/groups/${group?._id}/settings`, icon: <Settings />, privilege: 'admin' },
+    ]
+
+    const settingsItems = [
+        { name: "Edit profile", link: "/public_pages/Profile", icon: <Edit /> },
+        { name: "Logout", link: "#", icon: <LogOut /> },
     ]
     return (
         <>
@@ -48,6 +59,34 @@ function ChannelDetails({ }: Props) {
                         <PlusIcon />
                     </span>
                 </span>
+            </div>
+
+            <div className='absolute bottom-0 w-full bg-[#013a6f] h-auto p-5 flex items-center justify-start gap-2  '>
+                <Avatar>
+                    <AvatarImage src={currentUser?.profile_pic} className="bg-black w-[50px] h-[50px] rounded-full" />
+                    <AvatarFallback>{iconTextGenerator(currentUser?.firstName as string, currentUser?.lastName as string)}</AvatarFallback>
+                </Avatar>
+                <div className="flex items-center gap-2">
+                    <h1>{currentUser?.firstName} {currentUser?.lastName}</h1>
+                    <Popover>
+                        <PopoverTrigger>
+                            <Button>
+                                <Settings />
+                            </Button>
+                        </PopoverTrigger>
+
+                        <PopoverContent className="text-white bg-[#013a6f] shadow-2xl z-40 gap-1 flex flex-col border-transparent border-l-8 border-l-neutral-400 pl-3 ">
+                            {
+                                settingsItems.map((item, index) => (
+                                    <Link href={item.link} key={index} className="text-white flex p-2 w-full text-[1.1rem] hover:bg-[#6bb7ff73] cursor-pointer rounded-md items-center gap-2 duration-100">
+                                        {item.icon}
+                                        {item.name}
+                                    </Link>
+                                ))
+                            }
+                        </PopoverContent>
+                    </Popover>
+                </div>
             </div>
         </>
     )
