868dbf9f97f377ed26edb5323261391417d7132b
henritresor <shimwamanahenritresor@gmail.com>
2024-10-18 21:34:11 +0200

    modfied getting group members

--- src/app/groups/[groupId]/MemberList.tsx
+++ src/app/groups/[groupId]/MemberList.tsx
@@ -1,87 +1,115 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { GroupContext } from '@/context/GroupContext'
 import { iconTextGenerator } from '@/lib/iconTextGenerator'
-import { GroupTypes, User } from '@/types'
-import { CaretDownIcon } from '@radix-ui/react-icons'
+import { User } from '@/types'
 import { Search } from 'lucide-react'
 import React, { useContext, useEffect, useState } from 'react'
+import { toast } from 'sonner'
+import Cookies from 'js-cookie'
 
 type Props = {}
 
 function MemberList({ }: Props) {
 
     const { group } = useContext(GroupContext)
+    const [isLoading, setIsLoading] = useState(false)
+    const [admins, setAdmins] = useState<User[]>([]);
+    const [moderators, setModerators] = useState<User[]>([]);
+    const [members, setMembers] = useState<User[]>([]);
 
-    const [admins, setAdmins] = useState<{ users: User[]; role: string }[]>([]);
-    const [moderators, setModerators] = useState<{ users: User[]; role: string }[]>([]);
-    const [members, setMembers] = useState<{ users: User[]; role: string }[]>([]);
+    const handleGetMembers = async () => {
+        const token = Cookies.get('access-token')
+        try {
+            setIsLoading(true)
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/groups/members/${group?._id}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            const data = await res.json()
+            
+            console.log('members', data)
+            // Assuming the API returns the correct structure
+            setAdmins(data.members.filter((member: any) => member.role === "GroupManager"));
+            setModerators(data.members.filter((member: any) => member.role === "GroupModerator"));
+            setMembers(data.members.filter((member: any) => member.role === "GroupUser"));
+
+        } catch (error) {
+            toast.error("Something went wrong")
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
     useEffect(() => {
-        if (!group) return; // Prevents errors if group is null or undefined
-
-        console.log('group', group);
-
-        // Filter members based on their roles
-        const filteredAdmins = group.members.filter(member => member.role === 'GroupManager');
-        const filteredModerators = group.members.filter(member => member.role === 'GroupModerator');
-        const filteredMembers = group.members.filter(member => member.role === 'GroupUser');
-
-        setAdmins(filteredAdmins);
-        setModerators(filteredModerators);
-        setMembers(filteredMembers);
+        if (group) {
+            handleGetMembers()
+        }
     }, [group]);
 
     return (
         <div className='w-[25%] bg-[#013a6fa6]'>
+
+            {/* Search Bar */}
             <div className="flex items-center bg-[#013a6fae] p-2 justify-between text-neutral-200 w-full">
                 <input className='bg-transparent outline-none w-full' placeholder='Search ...' />
                 <Search />
             </div>
 
-            <div className="w-full p-2 mb-3">
-                <h1 className='p-1 font-bold text-[1.1rem] mb-2'>Admins</h1>
-                {
-                    admins.length === 0 ? "No admins" : admins.map((admin, index) => (
-                        <div key={index} className='flex items-center justify-normal gap-5 p-3'>
-                            <Avatar>
-                                <AvatarImage src={admin.users[0]?.profile_pic} className="bg-black" />
-                                <AvatarFallback>{iconTextGenerator(admin.users[0]?.firstName, admin.users[0]?.lastName)}</AvatarFallback>
-                            </Avatar>
-                            <h1>{admin.users[0]?.firstName} {admin.users[0]?.lastName}</h1>
-                        </div>
-                    ))
-                }
-            </div>
+          <>
+          {
+            isLoading ? ("loading ... ") :(
+                <>
+                            <div className="w-full p-2 mb-3">
+                                <h1 className='p-1 font-extrabold text-[0.9rem] mb-2 uppercase'>Admins</h1>
+                                {
+                                    admins.length === 0 ? "No admins" : admins.map((admin, index) => (
+                                        <div key={index} className='flex items-center justify-normal gap-5 p-3'>
+                                            <Avatar>
+                                                <AvatarImage src={admin.profile_pic} className="bg-black" />
+                                                <AvatarFallback>{iconTextGenerator(admin.firstName, admin.lastName)}</AvatarFallback>
+                                            </Avatar>
+                                            <h1>{admin.firstName} {admin.lastName}</h1>
+                                        </div>
+                                    ))
+                                }
+                            </div>
 
-            <div className="w-full p-2 mb-3">
-                <h1 className='p-1 font-bold text-[1.1rem] mb-2'>Moderators</h1>
-                {
-                    moderators.length === 0 ? "No moderators" : moderators.map((moderator, index) => (
-                        <div key={index} className='flex items-center justify-normal gap-5 p-3'>
-                            <Avatar>
-                                <AvatarImage src={moderator.users[0]?.profile_pic} className="bg-black" />
-                                <AvatarFallback>{iconTextGenerator(moderator.users[0]?.firstName, moderator.users[0]?.lastName)}</AvatarFallback>
-                            </Avatar>
-                            <h1>{moderator.users[0]?.firstName} {moderator.users[0]?.lastName}</h1>
-                        </div>
-                    ))
-                }
-            </div>
+                            <div className="w-full p-2 mb-3">
+                                <h1 className='p-1 font-extrabold text-[0.9rem] mb-2 uppercase'>Moderators</h1>
+                                {
+                                    moderators.length === 0 ? "No moderators" : moderators.map((moderator, index) => (
+                                        <div key={index} className='flex items-center justify-normal gap-5 p-3'>
+                                            <Avatar>
+                                                <AvatarImage src={moderator.profile_pic} className="bg-black" />
+                                                <AvatarFallback>{iconTextGenerator(moderator.firstName, moderator.lastName)}</AvatarFallback>
+                                            </Avatar>
+                                            <h1>{moderator.firstName} {moderator.lastName}</h1>
+                                        </div>
+                                    ))
+                                }
+                            </div>
 
-            <div className="w-full p-2 mb-3">
-                <h1 className='p-1 font-bold text-[1.1rem] mb-2'>Members</h1>
-                {
-                    members.length === 0 ? "No other members" : members.map((member, index) => (
-                        <div key={index} className='flex items-center justify-normal gap-5 p-3'>
-                            <Avatar>
-                                <AvatarImage src={member.users[0]?.profile_pic} className="bg-black" />
-                                <AvatarFallback>{iconTextGenerator(member.users[0]?.firstName, member.users[0]?.lastName)}</AvatarFallback>
-                            </Avatar>
-                            <h1>{member.users[0]?.firstName} {member.users[0]?.lastName}</h1>
-                        </div>
-                    ))
-                }
-            </div>
+                            <div className="w-full p-2 mb-3">
+                                <h1 className='p-1 font-extrabold text-[0.9rem] mb-2 uppercase'>All Members</h1>
+                                {
+                                    members.length === 0 ? "No other members" : members.map((member, index) => (
+                                        <div key={index} className='flex items-center justify-normal gap-5 p-3'>
+                                            <Avatar>
+                                                <AvatarImage src={member.profile_pic} className="bg-black" />
+                                                <AvatarFallback>{iconTextGenerator(member.firstName, member.lastName)}</AvatarFallback>
+                                            </Avatar>
+                                            <h1>{member.firstName} {member.lastName}</h1>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                </>
+            )
+          }
+          </>
+
         </div>
     )
 }
