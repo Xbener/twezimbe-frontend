c6520719d2b7b76aa50f06e8dba23f0fc2c79f49
henritresor <shimwamanahenritresor@gmail.com>
2024-10-23 00:23:45 +0200

    changed font

--- src/app/groups/direct/page.tsx
+++ src/app/groups/direct/page.tsx
@@ -5,7 +5,7 @@ type Props = {}
 function page({}: Props) {
   return (
       <div className='w-[100%] flex items-center justify-center h-full'>
-          <h1 className='text-[3rem] text-center'>
+          <h1 className='text-[2rem] text-center'>
               Welcome to Direct Messages. Select or start a conversation!
           </h1>
       </div>
