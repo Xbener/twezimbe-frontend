31db68644bd870f979a2b0e704cff929eb2472ba
henritresor <shimwamanahenritresor@gmail.com>
2024-11-13 15:51:21 +0200

    finished faqs

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,29 +1,25 @@
 'use client'
-import { useState } from 'react';
+import { getFaqs } from '@/api/inquries';
+import { FAQ } from '@/types';
+import { useEffect, useState } from 'react';
 
 interface FaqItem {
     question: string;
     answer: string;
 }
 
-const faqData: FaqItem[] = [
-    {
-        question: 'What is your return policy?',
-        answer: 'You can return any item within 30 days of purchase for a full refund.',
-    },
-    {
-        question: 'How do I track my order?',
-        answer: 'Once your order is shipped, you will receive an email with a tracking link.',
-    },
-    {
-        question: 'Do you ship internationally?',
-        answer: 'Yes, we offer worldwide shipping to most countries.',
-    },
-];
 
 const Faq = () => {
     const [openIndex, setOpenIndex] = useState<number | null>(null);
+    const [faqs, setFaqs] = useState<FAQ[]>([])
 
+    useEffect(() => {
+        const getData = async () => {
+            const res = await getFaqs()
+            setFaqs(res)
+        }
+        getData()
+    }, [])
     const toggleFaq = (index: number) => {
         setOpenIndex(openIndex === index ? null : index);
     };
@@ -32,7 +28,7 @@ const Faq = () => {
         <div className="max-w-2xl mx-auto my-8 p-4">
             <h2 className="text-2xl font-semibold mb-4 text-center">Frequently Asked Questions</h2>
             <ul>
-                {faqData.map((item, index) => (
+                {faqs.map((item, index) => (
                     <li key={index} className="border-b border-gray-300 py-4">
                         <button
                             onClick={() => toggleFaq(index)}
